796a0c0a43e9df60f98f299a2f2bd21dd6e30bb7
Julia Sylenok <silenokjulia1@gmail.com>
2023-12-22 15:52:21 +0200

    Store

---
+++ Project_partA_Sylenok/Store.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_partA_Sylenok
+{
+    public class Store
+    {
+        public class Store 
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public List<InventoryItem> Inventory { get; set; } = new List<InventoryItem>();
+
+            public Store(string name, string address)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void AddProductToInventory(ISellable product, int quantity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void PrintInventory()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void SellProduct(string productName, int quantity)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
